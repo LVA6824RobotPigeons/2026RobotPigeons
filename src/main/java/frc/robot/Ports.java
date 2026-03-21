41af9b6dfcb493895ae1ce763f07974a54f05970
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-03-21 11:34:57 -0700

    Fixes assorted stuff and adds more LED8 Implimentation

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.hardware.CANdle;
 import frc.robot.subsystems.led8.LED8;
+import frc.robot.subsystems.led8.LED8Group;
 
 public final class Ports {
 
@@ -12,21 +13,23 @@ public final class Ports {
 
     // CANdle
     public static final CANdle kCANdle = new CANdle(0,kRoboRioCANBus);
-    public static final LED8 kCandle = new LED8(kCANdle,Constants.LEDs.kNumberOfLights-1);
+    public static final LED8 kCandle = new LED8(kCANdle,0,7);
+    public static final LED8 kStrip = new LED8(kCANdle,8,167);
+    public static final LED8Group kLights = new LED8Group().add(kCandle).add(kStrip);
 
     // Talon FX IDs
-    public static final int kIntakePivot = -1;//10;
-    public static final int kIntakeRollers = -1;//11;
-    public static final int kFloor = -1;//12;
-    public static final int kFeeder = -1;//13;
-    public static final int kShooterLeft = -1;//14;
-    public static final int kShooterMiddle = -1;//15;
-    public static final int kShooterRight = -1;//16;
-    public static final int kHanger = -1;//18;
+    public static final int kIntakePivot = 10;
+    public static final int kIntakeRollers = 11;
+    public static final int kFloor = 12;
+    public static final int kFeeder = 13;
+    public static final int kShooterLeft = 14;
+    public static final int kShooterMiddle = 15;
+    public static final int kShooterRight = 16;
+    public static final int kHanger = 18;
 
     // PWM Ports
-    public static final int kHoodLeftServo = -1;//3;
-    public static final int kHoodRightServo = -1;//4;
+    public static final int kHoodLeftServo = 3;
+    public static final int kHoodRightServo = 4;
 
     // Controller Ports
     public static final int driver = 0;
