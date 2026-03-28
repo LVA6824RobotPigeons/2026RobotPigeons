613287f7c34086559c7218837b98a39fa98cf4be
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-03-28 13:14:12 -0700

    messed up thing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -68,7 +68,6 @@ public class Robot extends TimedRobot {
     }
     @Override
     public void teleopExit() {
-        LED8Implimentation.teleopMode();
         LED8Implimentation.teleopOff();
     }
 
