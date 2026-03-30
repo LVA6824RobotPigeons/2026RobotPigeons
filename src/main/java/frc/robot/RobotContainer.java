e70b031e13580e579465d39474e07a26947669c9
potatochicken120 <107384353+potatochicken120@users.noreply.github.com>
2026-03-30 10:02:23 -0700

    Robot testing(evan l)(from antirobot2) (#29)
    * Fixes assorted stuff and adds more LED8 Implimentation
    
    * Temporary sync
    
    * Temporary sync
    
    * Temporary sync
    
    * Temporary sync
    
    * edited code to match wcp codebase
    
    * started implemanting shooter toggle from max's
     code base
    
    * I merged changes I had made in a branched version
    of code.
    this code has manual shooter and feed working along with
    an untested manual impementation of the Hood
    (intake and climb also work)
    
    * fixed hood implementation
    
    * uodated choreo ChoreoLib2026
    
    * edited hood manual movement
    
    ---------
    
    Co-authored-by: Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
    Co-authored-by: Michael Beecher <56412184+JonDoeBeep@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -162,8 +162,8 @@ public class RobotContainer {
         driverPovUp().onTrue(hanger.positionCommand(Hanger.Position.HANGING));
         driverPovDown().onTrue(hanger.positionCommand(Hanger.Position.HUNG));
 
-        driverPovRight().onTrue(Commands.runOnce(() -> hood.cycleStage()));
-        driverPovLeft().onTrue(Commands.runOnce(() -> hood.cycleStage()));        
+        driverBButton().onTrue(Commands.runOnce(() -> hood.cycleStageUp()));
+        driverAButton().onTrue(Commands.runOnce(() -> hood.cycleStageDown()));        
     }
 
     private void configureManualDriveBindings() {
